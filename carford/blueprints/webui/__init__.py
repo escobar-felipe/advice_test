12a958770ba1f859c9e3d4c95f103d672c85508c
escobar-felipe <felipe_6512@hotmail.com>
2022-10-09 23:16:46 -0300

    fix: adjust templates

--- carford/blueprints/webui/__init__.py
+++ carford/blueprints/webui/__init__.py
@@ -1,13 +1,12 @@
 from flask import Blueprint
 
-from .views import index, vehicles
+from .views import index, owner_page
 
 bp = Blueprint("webui", __name__, template_folder="templates")
 
 bp.add_url_rule("/", view_func=index)
-bp.add_url_rule("/owner/<owner_id>", view_func=vehicles, endpoint="ownerview"
+bp.add_url_rule("/owner/<owner_id>", view_func=owner_page, endpoint="ownerview"
 )
-
 
 def init_app(app):
     app.register_blueprint(bp)
--- carford/blueprints/webui/views.py
+++ carford/blueprints/webui/views.py
@@ -4,15 +4,19 @@ from carford.ext.database import db
 from carford.ext.forms import Add_Owner, Add_Vehicle, Owners
 
 def index():
-    opportunities = Owner.query.join(Vehicle_owner, Vehicle_owner.id_owner==Owner.id , isouter=True).filter(Vehicle_owner.id_owner == None).all()
-    owners = Owner.query.join(Vehicle_owner,Vehicle_owner.id_owner==Owner.id).filter(Vehicle_owner.id_owner != None).distinct()
+    try:
+        opportunities = Owner.query.join(Vehicle_owner, Vehicle_owner.id_owner==Owner.id , isouter=True).filter(Vehicle_owner.id_owner == None).all()
+        owners = Owner.query.join(Vehicle_owner,Vehicle_owner.id_owner==Owner.id).filter(Vehicle_owner.id_owner != None).distinct()
+    except:
+        opportunities = []
+        owners = []        
     form_vehicle = Add_Vehicle()
     form_owner = Add_Owner()
     form_vehicle.owner.choices= [(owner.id,owner.name) for owner in opportunities]
     return render_template("index.html", opportunities=opportunities, owners=owners, form_owner=form_owner, form_vehicle=form_vehicle)
 
 
-def vehicles(owner_id):
+def owner_page(owner_id):
     vehicles_owners = db.session.query(Owner, Vehicle_owner, Vehicles).join(Vehicle_owner,Vehicle_owner.id_vehicle==Vehicles.id).join(Owner, Vehicle_owner.id_owner==Owner.id).filter(Vehicle_owner.id_owner == owner_id).all() or abort(
         404, "vehicles not finded"
     )
