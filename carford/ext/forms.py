2c2c431da282ce2d49218ec0fe2bb314febe7a66
escobar-felipe <felipe_6512@hotmail.com>
2022-10-09 23:20:48 -0300

    fix: add validators to forms

--- carford/ext/forms.py
+++ carford/ext/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,SelectField, StringField
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired
+
 
 class Add_Vehicle(FlaskForm):
-    color = SelectField("Color", choices=[("yellow","yellow"),("blue","blue"),("grey","grey")])
-    model = SelectField("Model",choices=[("hatch","hatch"),("sedan","sedan"),("convertible","convertible")])
-    brand = SelectField("Brand", choices=[("audi","audi"),("bmw","bmw"),("jeep","jeep"),("porshe","porshe")])
-    owner = SelectField("Owner",choices=[])
+    color = SelectField("Color", choices=[("yellow","yellow"),("blue","blue"),("grey","grey")],validators=[InputRequired()])
+    model = SelectField("Model",choices=[("hatch","hatch"),("sedan","sedan"),("convertible","convertible")],validators=[InputRequired()])
+    brand = SelectField("Brand", choices=[("audi","audi"),("bmw","bmw"),("jeep","jeep"),("porshe","porshe")],validators=[InputRequired()])
+    owner = SelectField("Owner",choices=[] ,validators=[InputRequired()])
 
 class Add_Owner(FlaskForm):
-    name= StringField("Name")
+    name= StringField("Name",validators=[InputRequired()])
 
 class Owners(FlaskForm):
-    owner = SelectField("Owner",choices=[])
+    owner = SelectField("Owner",choices=[], validators=[InputRequired()])
